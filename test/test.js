632b2350eb2108a82a202b8f7ec7a8e2ceaeb435
mariepascale <mp.chauvel@hotmail.com>
2016-03-23 22:03:58 -0400

    Removed node files

--- test/test.js
+++
@@ -1,8 +0,0 @@
-var request = require('supertest');
-var app = require('../app.js');
-
-describe('GET /', function() {
-	it('respond with Not Available', function(done) {
-		request(app).get('/').expect('Not Available', done);
-	});
-});
